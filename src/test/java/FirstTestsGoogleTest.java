20abaf0a41343ab52a592cc02cc8775e44f774d8
Mykola Sheremet <sheremetmykola@gmail.com>
2023-02-08 21:58:18 +0100

    second selenium lesson

--- src/test/java/FirstTestsGoogleTest.java
+++ src/test/java/FirstTestsGoogleTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -20,8 +21,13 @@ public class FirstTestsGoogleTest {
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("https://www.google.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 
     @Test
@@ -30,9 +36,18 @@ public class FirstTestsGoogleTest {
         Assert.assertEquals(title, "Google", "The page contains a wrong title");
     }
 
+    @Test
+    public void searchChecking() {
+        driver.findElement(By.id("L2AGLb")).click();
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("Tel-Ran");
+        driver.findElement(By.name("btnK")).click();
 
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
+        Assert.assertTrue(driver.getPageSource().contains("Tel-Ran.de"), "The expected text was not on the search page");
+
+        String actualText = driver.findElement(By.id("hdtb-tls")).getText();
+        String  expectedText = "Suchfilter";
+
+        Assert.assertEquals(actualText, expectedText, "Actual text does not match the expected text ");
     }
 }
--- src/test/java/PhoneBookFirstTest.java
+++ src/test/java/PhoneBookFirstTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -26,14 +27,8 @@ public class PhoneBookFirstTest {
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("http://phonebook.telran-edu.de:8080/user/login");
-    }
-
-    @Test
-    public void shouldBeRightTitle() {
-        title = driver.getTitle();
-        Assert.assertEquals(title, "PhonebookUi", "The page contains a wrong title");
     }
 
     @AfterMethod
@@ -41,4 +36,22 @@ public class PhoneBookFirstTest {
         driver.quit();
     }
 
+
+    @Test
+    public void shouldBeRightTitle() {
+        title = driver.getTitle();
+        Assert.assertEquals(title, "PhonebookUi", "The page contains a wrong title");
+    }
+
+    @Test
+    public void searchChecking() {
+        driver.findElement(By.name("email")).clear();
+        driver.findElement(By.name("email")).sendKeys("test@gmail.com");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("test@gmail.com");
+        driver.findElement(By.className("btn")).click();
+        driver.findElement(By.xpath("//button[contains(text(), 'Bakanov')]")).isDisplayed();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//button[contains(text(), 'Bakanov')]")).isDisplayed());
+    }
 }
